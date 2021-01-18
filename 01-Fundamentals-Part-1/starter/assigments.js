dd146fa6408fa03d0d3bfec5912988db96a6de90
SOMAN MORAES Carlos Eduardo (Carlao) <carlos.e.s.moraes@hexagon.com>
2021-01-18 10:07:01 -0300

    Assigments.js com tarefa 2

--- 01-Fundamentals-Part-1/starter/assigments.js
+++ 01-Fundamentals-Part-1/starter/assigments.js
@@ -1,6 +1,18 @@
+// Assigment 1: Values and Variables
+
 let country = 'Brasil';
 let continent = 'América do Sul';
 let population = 212000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Assigment 2: Data Types
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+//Assigment 3: let, const and var
