fd0cdb027793c68a415972555681ff85f1d74782
SOMAN MORAES Carlos Eduardo (Carlao) <carlos.e.s.moraes@hexagon.com>
2021-01-20 16:23:04 -0300

    Challenge 2

--- 01-Fundamentals-Part-1/starter/challenges.js
+++ 01-Fundamentals-Part-1/starter/challenges.js
@@ -1,4 +1,4 @@
-//Challenge 1
+/*Challenge 1
 let markWeight = 78;
 let markHeight = 1.69;
 let johnWeight = 92;
@@ -8,3 +8,16 @@ let johnBMI = johnWeight / johnHeight ** 2;
 let markhigherBMI = markBMI > johnBMI;
 console.log(markBMI, johnBMI);
 console.log(markhigherBMI);
+*/
+//Challenge 2
+const markWeight = 78;
+const markHeight = 1.69;
+const johnWeight = 92;
+const johnHeight = 1.95;
+let markBMI = markWeight / markHeight ** 2;
+let johnBMI = johnWeight / johnHeight ** 2;
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI (${markBMI.toFixed(1)}) is higher than John's (${johnBMI.toFixed(1)})`)
+} else {
+    console.log(`John's BMI (${johnBMI.toFixed(1)}) is higher than Mark's (${markBMI.toFixed(1)})`)
+}
