72f0bab4191c6f910cc48cd686cca872d62d7e7b
carlosesmoraes <carloscesm@gmail.com>
2021-03-18 15:42:45 -0300

    Challenge 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 //      Challenge 1
+/*
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 const avgDolphins = calcAverage(85, 53, 41);
 const avgKoalas = calcAverage(85, 54, 41);
@@ -15,4 +16,18 @@ const checkWinner = function (avgDolphins, avgKoalas) {
     return 0;
 }
 checkWinner(avgDolphins, avgKoalas);
-
+*/
+//      Challenge 2
+const calcTip = function (bill) {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15;
+    } else {
+        return bill * 0.20;
+    }
+}
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills);
+console.log(tips);
+console.log(totals);
