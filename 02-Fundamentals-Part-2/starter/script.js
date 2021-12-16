3396e6a86ea3161de4adc59f3cf10287a89b184a
carlosesmoraes <carloscesm@gmail.com>
2021-12-16 10:28:45 -0300

    Fim do Challenge 4

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -62,3 +62,32 @@ john.calcBMI();
 console.log(mark.bmi, john.bmi)
 */
 
+//Challenge 4
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15;
+    } else {
+        return bill * 0.20;
+    }
+}
+
+for (let i = 0; i < bills.length; i++) {
+    tips[i] = calcTip(bills[i]);
+    totals[i] = bills[i] + tips[i]
+}
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    let avg = sum / arr.length;
+    return avg;
+}
+console.log(bills)
+console.log(tips)
+console.log(totals)
+console.log(calcAverage(totals))
